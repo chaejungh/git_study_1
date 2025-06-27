8e9475166ffdac11b395fe2942529625e39b9ab2
chaejungho <chaejungh@gmail.com>
2025-06-27 12:59:18 +0900

    ouput haha작성

--- src/Login.java
+++ src/Login.java
@@ -1,7 +1,11 @@
 public class Login {
+
     //로그인 기능 구현 완료!!
     //비밀번호 저장
     //자동 로그인
     //이메일 인증
     //전화 문자 인증
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
 }
